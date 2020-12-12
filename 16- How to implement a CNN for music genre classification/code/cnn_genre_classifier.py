684b06c463533322680abb3d04e6ca92649cef4c
varun-ms-s <65679176+varun-ms-s@users.noreply.github.com>
2020-12-12 12:09:24 +0530

    Update cnn_genre_classifier.py

--- 16- How to implement a CNN for music genre classification/code/cnn_genre_classifier.py
+++ 16- How to implement a CNN for music genre classification/code/cnn_genre_classifier.py
@@ -110,7 +110,7 @@ def build_model(input_shape):
     model.add(keras.layers.Dropout(0.3))
 
     # output layer
-    model.add(keras.layers.Dense(10, activation='softmax'))
+    model.add(keras.layers.Dense(5, activation='softmax'))
 
     return model
 
